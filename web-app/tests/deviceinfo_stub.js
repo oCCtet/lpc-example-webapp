14d8bf77535e2a99ce970fccc7566a0a51071f19
Sami Hartikainen <sami.hartikainen@teleste.com>
2015-09-28 16:37:42 +0300

    Revise tests

--- web-app/tests/deviceinfo_stub.js
+++ web-app/tests/deviceinfo_stub.js
@@ -4,12 +4,15 @@
 // Copyright (C) 2015 Teleste Corporation
 
 var fs = require("fs");
+var parser = require("body-parser");
 var express = require("express");
 var app = express();
 
 // Set to 'true' to make the stub report errors
 // for requests.
 var reportError = false;
+
+app.use(parser.json());
 
 app.get("/sensors/temperature", function (req, res) {
     if (reportError === true) {
@@ -34,8 +37,7 @@ app.post("/input/streaming", function (req, res) {
     if (reportError === true) {
 	res.status(500).json({ id: -1, error: "Internal error", details: "reportError === true" });
     } else {
-	var input = JSON.parse(req.body);
-	if (input.addr === undefined || input.vlan === undefined) {
+	if (req.body.addr === undefined || req.body.vlan === undefined) {
 	    res.status(400).json({ id: -1, error: "Bad request", details: "request body must contain 'addr' and 'vlan' tokens" });
 	} else {
 	    // client ignores the success response, so we
@@ -56,8 +58,8 @@ app.delete("/input/streaming", function (req, res) {
 app.delete("/input/streaming/:id", function (req, res) {
     if (reportError === true) {
 	res.status(500).json({ id: -1, error: "Internal error", details: "reportError === true" });
-    } else if (parseInt(req.params.id) !== 99999) {
-	res.status(400).json({ id: -1, error: "Bad request", details: "id must be the special token '99999'" });
+    } else if (req.params.id !== "byquery") {
+	res.status(400).json({ id: -1, error: "Bad request", details: "id must be the special token 'byquery'" });
     } else if (req.query.addr === undefined || req.query.vlan === undefined) {
 	res.status(400).json({ id: -1, error: "Bad request", details: "query string must contain 'addr' and 'vlan' tokens" });
     } else {
